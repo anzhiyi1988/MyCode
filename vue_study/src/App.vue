e950c83e4d8b1f39601ad35d6557389649feb404
anzhy <anzhy@thunisoft.com>
2019-10-10 10:17:33 +0800

    提交vue学习代码

---
+++ vue_study/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <div>
+            <input type="text" v-model="in_value"/>
+            <button v-on:click="addValue">add value</button>
+        </div>
+        <ul>
+            <TodoLi v-for="v in in_values" v-bind:value="v">
+                <template v-slot:todo-li-slot>
+                    <span>{{v}}</span>
+                </template>
+            </TodoLi>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+    import TodoLi from "./components/TodoLi";
+
+    export default {
+        name: 'app',
+        components: {
+            TodoLi,
+            HelloWorld
+        },
+        data() {
+            return {
+                in_value: '',
+                in_values: []
+            }
+        },
+        methods: {
+            addValue() {
+                console.log(this.in_value);
+                this.in_values.push(this.in_value);
+                this.in_value = '';
+            }
+
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ vue_study/src/components/TodoLi.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        <slot name="todo-li-slot"></slot>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "TodoLi",
+        props: ['value']
+    }
+</script>
+
+<style scoped>
+
+</style>
